9ac79867502fc20565ed04d62abff37a1fe842a3
Abdullah Al Mamoon <mamoon036@gmail.com>
2022-02-27 01:51:24 +0600

    27.02.2022

--- functions.php
+++ functions.php
@@ -1,7 +1,15 @@
 <?php 
 
 	function university_files() {
-		wp_enqueue_style( 'university_main_style', get_stylesheet_uri() );
+		wp_enqueue_script( 'university_slide_scripts', get_theme_file_uri( '/build/index.js' ), array(), wp_get_theme()->get('Version'), true );
+
+		wp_enqueue_style( 'university-google-font', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+
+		wp_enqueue_style( 'university_fontawesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css', [], [] );
+
+		wp_enqueue_style( 'university_index_style', get_template_directory_uri().'/build/index.css', [], '' );
+
+		wp_enqueue_style( 'university_main_style', get_stylesheet_uri(), [], filemtime( get_template_directory().'/style.css' ) );
 	}
 
 	add_action( 'wp_enqueue_scripts', 'university_files' );

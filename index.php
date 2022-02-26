a508480eabd01983f1488b5dcc1a5525696f4aa7
Abdullah Al Mamoon <mamoon036@gmail.com>
2022-02-26 23:36:54 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+	get_header();
+
+	while ( have_posts() ) {
+		the_post();
+		?>
+		<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+		<p><?php the_content(); ?></p>
+		<hr>
+		<?php
+	}
+
+	get_footer();
+?>

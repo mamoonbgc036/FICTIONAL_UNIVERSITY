5a9339ece05fb61c668efeafe4786309c8b95a78
Abdullah Al Mamoon <mamoon036@gmail.com>
2022-02-28 08:58:49 +0600

    28.02.2022

--- functions.php
+++ functions.php
@@ -14,4 +14,11 @@
 
 	add_action( 'wp_enqueue_scripts', 'university_files' );
 
+
+	function university_support() {
+		add_theme_support( 'title-tag' );
+	}
+
+	add_action( 'after_setup_theme', 'university_support' );
+
 ?>
--- page.php
+++ page.php
@@ -1,10 +1,68 @@
 <?php 
+	get_header();
+
 	while ( have_posts() ) {
 		the_post();
 		?>
-		<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-		<p><?php the_content(); ?></p>
-		<hr>
+		 <div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_template_directory_uri() . '/images/ocean.jpg' ?>)"></div>
+      <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title"><?php the_title() ?></h1>
+        <div class="page-banner__intro">
+          <p>Learn how the school of your dreams got started.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="container container--narrow page-section">
+    	<?php 
+    		$parent_id = wp_get_post_parent_id( get_the_ID() );
+    		if ( $parent_id ) {
+    			?>
+    			<div class="metabox metabox--position-up metabox--with-home-link">
+			        <p>
+			          <a class="metabox__blog-home-link" href="<?php echo get_permalink( $parent_id ); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title( $parent_id ); ?></a> <span class="metabox__main"><?php the_title(); ?></span>
+			        </p>
+			   	</div>
+    			<?php
+    		}
+    	?>
+      
+      <?php 
+      	// Check whether the current page has childs
+      	$has_childs = get_pages( array(
+      		'child_of' => get_the_ID(),
+      	) );
+
+      	if ( $parent_id || $has_childs ) {
+
+      ?>
+      <div class="page-links">
+        <h2 class="page-links__title"><a href="<?php echo get_permalink( $parent_id ) ?>"><?php echo get_the_title( $parent_id ); ?></a></h2>
+        <ul class="min-list">
+          <?php 
+
+          	 if ( $parent_id ) {
+          	 	$find_children_of = $parent_id;
+          	 } else {
+          	 	$find_children_of = get_the_ID();
+          	 }
+	         wp_list_pages( array(
+		        'title_li' => NULL,
+		        'child_of' => $find_children_of,
+		     ) );
+
+          ?>
+        </ul>
+      </div>
+
+  	  <?php } ?>
+
+      <div class="generic-content">
+        <?php the_content(); ?>
+      </div>
+    </div>
 		<?php
+		get_footer();
 	}
 ?>

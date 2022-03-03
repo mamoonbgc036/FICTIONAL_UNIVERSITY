d8a103042d4dd7613b607584bc257770428b4a45
Abdullah Al Mamoon <mamoon036@gmail.com>
2022-03-03 08:41:37 +0600

    03.03.2022

--- archive.php
+++ archive.php
@@ -4,9 +4,9 @@ get_header();
  <div class="page-banner">
       <div class="page-banner__bg-image" style="background-image: url(<?php echo get_template_directory_uri() . '/images/ocean.jpg' ?>)"></div>
       <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title">Welcome to Our Blog Section!!!</h1>
+        <h1 class="page-banner__title"><?php the_archive_title(); ?></h1>
         <div class="page-banner__intro">
-          <p>Keep up with our blog..</p>
+          <p><?php the_archive_description(); ?></p>
         </div>
       </div>
   </div>

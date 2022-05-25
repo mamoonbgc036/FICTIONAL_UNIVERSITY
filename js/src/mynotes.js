4903329c2fec224c348c2b1565ffed7642ec3b90
Abdullah Al Mamoon <mamoon036@gmail.com>
2022-05-25 09:10:24 +0600

    25.04.2022

--- js/src/mynotes.js
+++ js/src/mynotes.js
@@ -8,13 +8,30 @@ class MyNotes{
 
 	events(){
 		$( '.delete-note' ).on( 'click', this.deleteNote );
-		$( '.edit-note' ).on( 'click', this.editNote );
+		$( '.edit-note' ).on( 'click', this.editNote.bind(this) );
 	}
 
 	editNote(e){
 		let edit = $(e.target).parents('li');
+		if( edit.data('state')=='editable' ){
+			this.makeNoteReadonly(edit);
+		} else{
+			this.makeNoteEditable(edit);
+		}
+	}
+
+	makeNoteEditable(edit){
 		edit.find('.note-title-field, .note-body-field').removeAttr('readonly').addClass('note-active-field');
+		edit.find('.edit-note').html('<i class="fa fa-times"></i>cancel')
 		edit.find( '.update-note' ).addClass('update-note--visible');
+		edit.data('state', 'editable');
+	}
+
+	makeNoteReadonly(edit){
+		edit.find('.note-title-field, .note-body-field').attr('readonly', 'readonly').removeClass('note-active-field');
+		edit.find('.edit-note').html('<i class="fa fa-pencil"></i>edit')
+		edit.find( '.update-note' ).removeClass('update-note--visible');
+		edit.data('state', 'cancel');
 	}
 
 	deleteNote(e){
